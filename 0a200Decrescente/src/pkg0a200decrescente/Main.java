0660083e10c786e5e28900790750e68aa615707f
Thiago Yugue Kono Campos <106500654+kono100@users.noreply.github.com>
2023-03-25 14:50:36 -0300

    Add files via upload

---
+++ 0a200Decrescente/src/pkg0a200decrescente/Main.java
@@ -0,0 +1,12 @@
+package pkg0a200decrescente;
+/*7) Faça um algoritmo que imprima os valores de 0 a 200 de forma decrescente.*/
+public class Main {
+
+    public static void main(String[] args) {
+
+    for (int i = 200; i >= 0; i--) {
+
+    System.out.println(i);
+}
+}
+}
